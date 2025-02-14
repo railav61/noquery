06961041bd8d4b458e5ec4164f9032c744df89fd
Mritunjay Rai <146355352+railav61@users.noreply.github.com>
2025-02-14 18:20:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const { verifyToken } = require("./middleware/auth");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 const MONGOURL = process.env.MONGOURL;
